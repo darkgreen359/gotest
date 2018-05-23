4ec4d0c7b66bbcdc713e94c3f70c196d9f149ef1
Kiichi Yamato <kyamato@idcf.jp>
2018-05-23 16:32:00 +0900

    modify just a message 2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example YAMATO"
+	title := "Jenkins X golang http example YAMATO 2"
 
 	from := ""
 	if r.URL != nil {
